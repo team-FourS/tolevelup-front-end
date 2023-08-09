cce3a4982a4470ed7631be3479f54c65c79ae4f2
seyeon park <118905840+seyeonparkpark@users.noreply.github.com>
2023-08-09 18:15:20 +0900

    세연랭킹화면수정

--- src/component/Ranking/RankAll.js
+++ src/component/Ranking/RankAll.js
@@ -1,13 +1,45 @@
 import React from "react";
-// import Header from "../component/Header";
-// import Footer from "../component/Footer";
+import user from '../../img/user.png';
+import RankingNum1 from '../../img/medal1.png';
+import RankingNum2 from '../../img/medal2.png';
+import RankingNum3 from '../../img/medal3.png';
+
 import "../../css/Fifth.css";
 
 const RankAll = () => {
     return (
 <main className="layout_fifth">
-  <div>
-        첫번째      
+  <div className="rank_block">
+   <div className="rank_locate">
+    <img className ="ranking_number" src={RankingNum1} alt='프로필' />
+      <img className ="rank_user_profile" src={user} alt='프로필' />
+      <div className="rank_Info">
+        <h4 className="rank_user_name"> Lv2. 여기는 전체 </h4> 
+        <h4 className="rank_user_id">매일을 성실하게!!</h4>
+        
+      </div>      
+  </div>
+  </div>
+  <div className="rank_block">
+  <div className="rank_locate">
+    <img className ="ranking_number" src={RankingNum2} alt='프로필' />
+      <img className ="rank_user_profile" src={user} alt='프로필' />
+      <div className="rank_Info">
+        <h4 className="rank_user_name"> Lv2. 여기는 전체 </h4> 
+        <h4 className="rank_user_id">매일을 성실하게!!</h4>
+      </div>      
+  </div>
+  </div>
+  <div className="rank_block">
+  <div className="rank_locate">
+    <img className ="ranking_number" src={RankingNum3} alt='프로필' />
+      <img className ="rank_user_profile" src={user} alt='프로필' />
+      <div className="rank_Info">
+        <h4 className="rank_user_name"> Lv2. 여기는 전체 </h4> 
+        <h4 className="rank_user_id">매일을 성실하게!!</h4>
+        
+      </div>      
+  </div>
   </div>
     </main>
           
