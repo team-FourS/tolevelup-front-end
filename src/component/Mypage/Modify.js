fad5e99e4465fa6be9ad1906f84a96deb59a2b51
khoikangim <lina0430@naver.com>
2023-08-16 14:16:52 +0900

    피드 좋아요 아이콘 추가

--- src/component/Mypage/Modify.js
+++ src/component/Mypage/Modify.js
@@ -22,26 +22,26 @@ height : 100%;
 export const ModalBackdrop = styled.div`
   // Modal이 떴을 때의 배경을 깔아주는 CSS를 구현
   z-index: 1; //위치지정 요소
-  position: fixed;
-  display : flex;
-  justify-content : center;
-  align-items : center;
-  background-color: rgba(0,0,0,0.4);
-  border-radius: 10px;
-  top : 0;
-  left : 0;
-  right : 0;
-  bottom : 0;
+    position: fixed;
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    background-color: rgba(0,0,0,0.4);
+    border-radius: 10px;
+    top : 0;
+    left : 0;
+    right : 0;
+    bottom : 0;
 `;
 
 export const ModalBtn = styled.button`
-  background-color: var(--coz-purple-600);
-  text-decoration: none;
-  border: none;
-  padding: 20px;
-  color: white;
-  border-radius: 30px;
-  cursor: grab;
+    background-color: var(--coz-purple-600);
+    text-decoration: none;
+    border: none;
+    padding: 20px;
+    color: white;
+    border-radius: 30px;
+    cursor: grab;
 `;
 
 export const ExitBtn = styled(ModalBtn) `
@@ -59,20 +59,20 @@ align-items : center;
 
 export const ModalView = styled.div.attrs((props) => ({
   // attrs 메소드를 이용해서 아래와 같이 div 엘리먼트에 속성을 추가할 수 있다.
-  role: 'dialog',
+    role: 'dialog',
 }))`
   // Modal창 CSS를 구현합니다.
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  border-radius: 20px;
-  width: 500px;
-  heigth: 200px;
-  background-color: #ffffff;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    border-radius: 20px;
+    width: 500px;
+    heigth: 200px;
+    background-color: #ffffff;
     >div.desc {
-      margin: 50px;
-      font-size: 20px;
-      color: var(--coz-purple-600);
+        margin: 50px;
+        font-size: 20px;
+        color: var(--coz-purple-600);
     }
 `;
 
@@ -80,11 +80,11 @@ const Modify = () => {
 
     const [isOpen, setIsOpen] = useState(false);
 
-  const openModalHandler = () => {
+    const openModalHandler = () => {
     // isOpen의 상태를 변경하는 메소드를 구현
     // !false -> !true -> !false
     setIsOpen(!isOpen) 
-  };
+    };
     const [editprofile, setprofile] = useState(false);
 
     const useConfirm = (message = null, onConfirm, onCancel) => {
