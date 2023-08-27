724be22d2d05fc67d638070888e983695f51d284
khoikangim <lina0430@naver.com>
2023-08-28 03:47:24 +0900

    피드 체크박스 수정 중

--- src/component/Feed/AllFeed.js
+++ src/component/Feed/AllFeed.js
@@ -1,43 +1,37 @@
-import React, { useState }  from "react";
+import React, { useState } from "react";
 import Header from "../../component/Header/Header";
 import Footer from "../../component/Footer";
 import FollowFeed from "./FollowFeed";
 import "../../css/feed/AllFeed.css";
-import {Routes, Route, Link} from "react-router-dom";
-import user from '../../img/user.png';
+import { Routes, Route, Link } from "react-router-dom";
+import user from "../../img/user.png";
 
-import {BiSearchAlt2} from "react-icons/bi";
-import {HiHeart} from "react-icons/hi";
-import {LiaCommentSolid} from "react-icons/lia";
-import { FaCheck } from 'react-icons/fa';
+import { BiSearchAlt2 } from "react-icons/bi";
+import { HiHeart } from "react-icons/hi";
+import { LiaCommentSolid } from "react-icons/lia";
 
 import CommentModal from "./CommentModal";
 
+
 const AllFeed = () => {
   const [isActive, setIsActive] = useState(false);
+
   const FeedClick = () => {
     setIsActive(!isActive);
   };
   const [isModalOpen, setIsModalOpen] = useState(false);
+
   const handleCommentIconClick = () => {
-    setIsModalOpen(!isModalOpen);
+    setIsModalOpen(!isModalOpen); // 댓글 창 열고 닫기
   };
+
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
-  const handleCommentSubmit = (comment) => {
+
+  const handleCommentSubmit = (comment) => { //댓글 창 닫기
     handleCloseModal();
   };
-
-  const todoItems = [
-    {
-      mission: "운동 | 줄넘기 100회 하기",
-      user: "Lv2. 여기는 전체",
-      content: "매일을 성실하게!!",
-      checked: true, //항상 true
-    },
-    //추가적인 할 일 항목
-  ];
 
   return (
     <div className="layout_feed">
@@ -52,6 +46,7 @@ const AllFeed = () => {
           className={`allFeed ${isActive ? 'allfeed_active' : ''}`}
           onClick={FeedClick}> 전체 </button>
         </Link>
+
 
         <Link to ="/FollowFeed">
         <button
@@ -68,50 +63,40 @@ const AllFeed = () => {
           </div>    
         </label>
 
+        <div className="feed_scrollbox">
 
-      <div className="feed_scrollbox">
-        {todoItems.map((todo, index) => (
-          <div className="feedBox01" key={index}>
-            <img className="user_profile" src={user} alt="프로필" />
-            <div className="feedContent">
-              <div className="userInfo">
-                <h4>{todo.user}</h4>
-                <p className="oneLine">{todo.content}</p>
-              </div>
-
-              <div className="feedChecklist">
-                <input
-                  type="checkbox"
-                  id={`btn1-${index}`}
-                  checked={todo.checked}
-                />
-                <label htmlFor={`btn1-${index}`}>
-                  <span className="feed-checkbox">
-                    {todo.checked && <FaCheck className="check-icon" style={{ color: 'green' }} />}
-                  </span>
-                  {todo.mission}
-                </label>
-                {/* 추가적인 mission 체크박스와 레이블 */}
-              </div>
-
-              <HiHeart className="heart_icon" />
-              <LiaCommentSolid
-                className="comment_icon"
-                onClick={handleCommentIconClick}
-              />
-              {isModalOpen && (
-                <CommentModal
-                  onClose={handleCloseModal}
-                  onSubmit={handleCommentSubmit}
-                />
-              )}
+        <div className="feedBox01">
+          <img className="user_profile" src={user} alt="프로필" />
+          <div className="feedContent">
+            <div className="userInfo">
+              <h4> Lv2. 여기는 전체 </h4>
+              <p className="oneLine">매일을 성실하게!!</p>
             </div>
-          </div>
-        ))}
+
+          <div className="feed_checkbox-container">
+              <input type="checkbox" id="btn1" className="feed_checkbox" />
+              <label htmlFor="btn1" className="feed_todo-text">
+                식습관 | 밀가루 음식 먹지 않기
+              </label> <br />
+            </div>
+
+          <HiHeart className="heart_icon" />
+          <LiaCommentSolid
+            className="comment_icon"
+            onClick={handleCommentIconClick}
+          />
+          {isModalOpen && (
+            <CommentModal
+              onClose={handleCloseModal}
+              onSubmit={handleCommentSubmit}
+            />
+          )}
+        </div>
+      </div>
+
       </div>
       <Footer/>
-    </div>
+    </div>  
   );
-};
-
+}
 export default AllFeed;
