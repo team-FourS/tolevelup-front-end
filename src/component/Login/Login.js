5b01e925b67814969582fd7c329529fc4ce88c6f
seyeon park <118905840+seyeonparkpark@users.noreply.github.com>
2023-09-19 16:51:31 +0900

    axios로그인 테스트

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -31,36 +31,46 @@ function Login() {
     console.log("click login");
     console.log("ID : ", inputId);
     console.log("PW : ", inputPw);
-    axios
-      .post(URL, {
+    axios({
+      url:'api/v1/users/login',
+      method: 'POST',
+      data: {
         id: inputId,
         password: inputPw,
+      },
       })
       .then((res) => {
-        console.log(res);
-        console.log("res.data.userId :: ", res.data.userId);
-        console.log("res.data.msg :: ", res.data.msg);
-        if (res.data.id === undefined) {
-          // id 일치하지 않는 경우 userId = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
-          console.log("======================", res.data.msg);
-          alert("입력하신 id 가 일치하지 않습니다.");
-        } else if (res.data.id === null) {
-          // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
-          console.log(
-            "======================",
-            "입력하신 비밀번호 가 일치하지 않습니다."
-          );
-          alert("입력하신 비밀번호 가 일치하지 않습니다.");
-        } else if (res.data.id === inputId) {
-          // id, pw 모두 일치 userId = userId1, msg = undefined
-          console.log("======================", "로그인 성공");
-          sessionStorage.setItem("user_id", inputId); // sessionStorage에 id를 user_id라는 key 값으로 저장
-          sessionStorage.setItem("name", res.data.name); // sessionStorage에 id를 user_id라는 key 값으로 저장
-        }
-        // 작업 완료 되면 페이지 이동(새로고침)
-        document.location.href = "/Mission";
+        localStorage.setItem('token',res.data.result.token);
+        console.log(res.data.result.token);
+        document.location.href = "/Mission"
+        // console.log(res);
+        // console.log("res.data.userId :: ", res.data.userId);
+        // console.log("res.data.msg :: ", res.data.msg);
+        // if (res.data.id === undefined) {
+        //   // id 일치하지 않는 경우 userId = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
+        //   console.log("======================", res.data.msg);
+        //   alert("입력하신 id 가 일치하지 않습니다.");
+        // } else if (res.data.id === null) {
+        //   // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
+        //   console.log(
+        //     "======================",
+        //     "입력하신 비밀번호 가 일치하지 않습니다."
+        //   );
+        //   alert("입력하신 비밀번호 가 일치하지 않습니다.");
+        // } else if (res.data.id === inputId) {
+        //   // id, pw 모두 일치 userId = userId1, msg = undefined
+        //   console.log("======================", "로그인 성공");
+        //   sessionStorage.setItem("user_id", inputId); // sessionStorage에 id를 user_id라는 key 값으로 저장
+        //   sessionStorage.setItem("name", res.data.name); // sessionStorage에 id를 user_id라는 key 값으로 저장
+        //   document.location.href = "/Mission";
+        // }
+        // // 작업 완료 되면 페이지 이동(새로고침)
+        // document.location.href = "/";
+
       })
-      .catch();
+      .catch((error)=>{
+        console.log(error);
+      });
   };
 
   return (
