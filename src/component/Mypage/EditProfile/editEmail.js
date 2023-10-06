dac18f012bd23bc7e3ea61ec69dfe789fe616026
khoikangim <lina0430@naver.com>
2023-10-07 05:12:49 +0900

    이메일 수정 화면 구현 완료

--- src/component/Mypage/EditProfile/editEmail.js
+++ src/component/Mypage/EditProfile/editEmail.js
@@ -6,24 +6,26 @@ import {Link} from "react-router-dom";
 const editEmail = () => {
 
 return (
-    <body className='check_body'>
-        <div className="layout_check">
-            <div className="checkForm">
-                <p className='user_check'>사용자 확인</p>
-                <p>* 변경할 이메일 주소를 입력해주세요</p>
-                    <TextField className="editField"
-                        label="Secure"
+    <body className='check_body1'>
+      <main className="check_main1">
+        <div className="layout_check1">
+            <div className="checkForm1">
+                <p className='user_check1'>이메일 변경</p>
+                <p className='editExplain'>* 변경할 이메일 주소를 입력해주세요</p>
+                    <TextField className="editField1"
+                        placeholder='이메일'
                         name="Secure"
                         autoComplete="Secure"
                         autoFocus />
                 <Link to="/modify">
-                    <button className='check_button'>확인</button>
+                    <button className='check_button1'>확인</button>
                 </Link>
-                <Link to="/mypage">
-                    <button className='check_button'>취소</button>
+                <Link to="/modify">
+                    <button className='check_button1'>취소</button>
                 </Link>
             </div>
         </div>
+        </main>
     </body>
 )
 }
