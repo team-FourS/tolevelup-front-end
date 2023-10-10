2c3f5a2d214d8d2eaa415d34ee4775552b5b585e
khoikangim <lina0430@naver.com>
2023-10-10 19:38:42 +0900

    서현 - 운동 미션 체크 저장1

--- src/component/Mission/MissionCheck01.js
+++ src/component/Mission/MissionCheck01.js
@@ -70,7 +70,7 @@ function MissionCheck01() {
     <div>
       <div className="checklist-border">
         <button className="btnMissionCheck">운동</button>
-        <div className="missionList3">
+        <div className="missionList">
           <ul>
             {missions.map((mission) => (
               <TodoItem
