c8fc2b74359d5420a3f404306a29b3769c0265ed
seyeon park <118905840+seyeonparkpark@users.noreply.github.com>
2023-10-11 15:48:06 +0900

    세연 통계오류수정

---
+++ src/component/Record/GetComment.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecordGetComment = () => {
+  return(
+    <div>
+      <p>여기는 받은 코멘트</p>
+    </div>
+
+  );
+}
+export default RecordGetComment;
---
+++ src/component/Record/RecordRanking.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecordRanking = () => {
+  return(
+    <div>
+      <p>여기는 랭킹</p>
+    </div>
+
+  );
+}
+export default RecordRanking;
