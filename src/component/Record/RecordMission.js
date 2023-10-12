06a0d71984cf20c90fa3ea04ec9c80bc7c061780
khoikangim <lina0430@naver.com>
2023-10-12 19:17:19 +0900

    머지 중 코드 오류 수정 완료

--- src/component/Record/RecordMission.js
+++ src/component/Record/RecordMission.js
@@ -3,44 +3,24 @@ import React, { useState, useEffect } from 'react';
 import "../../css/record/RecordMission.css"
 
 const RecordMission = () => {
-<<<<<<< HEAD
   const [totalCount, setTotalCount] = useState(0);
   const [exerciseCount, setExerciseCount] = useState(0);
   const [eatCount, setEatCount] = useState(0);
   const [cultureCount, setCultureCount] = useState(0);
   const [hobbyCount, setHobbyCount] = useState(0);
-=======
-  const [totalCount, setTotalCount] = useState('');
-  // const [exerciseCount, setExerciseCount] = useState([]);
-  // const [eatCount, setEatCount] = useState([]);
-  // const [cultureCount, setCultureCount] = useState([]);
-  // const [hobbyCount, setHobbyCount] = useState([]);
->>>>>>> db3c58f79ee2f034de016c87e1b9d9a228d2e205
 
   useEffect(() => {
 
     // 전체 누적 미션 개수 가져오기
     axiosInstance.get('api/v1/users/missions/counts')
     .then((res) => {
-<<<<<<< HEAD
       setTotalCount(res.data.result);
       console.log(res.data);
-=======
-
-      console.log(res.data);
-
-      const totalData = res.data.result;
-      setTotalCount(totalData);
->>>>>>> db3c58f79ee2f034de016c87e1b9d9a228d2e205
     })
 
     .catch((error) => {
       console.log('Failed to fetch user info:', error);
     });
-<<<<<<< HEAD
-=======
-  }, []);
->>>>>>> db3c58f79ee2f034de016c87e1b9d9a228d2e205
 
     // 테마 별 누적 미션 개수 가져오기
     axiosInstance.get('api/v1/users/missions/themes/counts')
@@ -73,11 +53,7 @@ const RecordMission = () => {
       
   return(
     <div>
-<<<<<<< HEAD
-      <p className="mission_totalCount">누적 미션 수행 : {totalCount} 개</p>
-=======
       <p className="mission_totalCount">누적 미션 수행: {totalCount}개</p>
->>>>>>> db3c58f79ee2f034de016c87e1b9d9a228d2e205
         <div className="mission_parent01">
           <div className="ExerciseCount">
             <p className="mission_theme">운동</p>
